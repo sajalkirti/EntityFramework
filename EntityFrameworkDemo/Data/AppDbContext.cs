bb8517db4383c47061453296fe0677483c41f22c
sajal Kirtiman <sajalkirti@gmail.com>
2025-01-03 16:44:25 +0530

    db connection and context

---
+++ EntityFrameworkDemo/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameworkDemo.Data
+{
+	public class AppDbContext : DbContext
+	{
+		public AppDbContext(DbContextOptions dbContextOptions):base(dbContextOptions) 
+		{ 
+		}
+	}
+}
